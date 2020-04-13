77325b9065d33e541fcb0bf34bcbcf47c3aac1d4
Denis Ivanov <ltybc3528@mail.ru>
2020-04-13 11:52:17 +0300

    Добавлена авторизация и регистрация пользователей

---
+++ RentApartment/Models/ApplicationUserManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace RentApartment.Models
+{
+    //вся работа с пользователями идет не напрямую, а через менеджер пользователей
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options,
+                                                IOwinContext context)
+        {
+            ApplicationContext db = context.Get<ApplicationContext>();
+            ApplicationUserManager manager = new ApplicationUserManager(new UserStore<ApplicationUser>(db));
+            return manager;
+        }
+    }
+}
